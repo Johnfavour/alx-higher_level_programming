226a2e7e6a9bb91dadc3f727fe70e57428dd82d4
Johnfavour <favourjohn851@gmail.com>
2023-05-24 15:04:29 -0700

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
+const file = process.argv[2];
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+
+fs.readFile(file, 'utf8', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
 });
